a5513909d47fb15a14a83061559b6b424491ddda
bhanage-viraj <bhanagevirajvaibahv@gmail.com>
2025-02-21 19:42:29 +0530

    navigation stack of login to dashboard

--- Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/DashboardView.swift
+++ Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/DashboardView.swift
@@ -5,3 +5,29 @@
 //  Created by Viraj Bhanage on 18/02/25.
 //
 
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Button(action: {
+                print("Attendance Marked")
+            }) {
+                Text("Mark Attendance")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding()
+            }
+        }
+        .padding()
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
--- Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/LoginView.swift
+++ Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/LoginView.swift
@@ -5,3 +5,38 @@
 //  Created by Viraj Bhanage on 18/02/25.
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    @State private var isLoggedIn = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Button(action: {
+                    isLoggedIn = true
+                }) {
+                    Text("Login")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding()
+                }
+                
+                // NavigationLink to DashboardView
+                NavigationLink(destination: DashboardView(), isActive: $isLoggedIn) {
+                    EmptyView()
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
