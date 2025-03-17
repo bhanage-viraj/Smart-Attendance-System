28bcb578e20c0a21295bf8e1c29217e406157241
bhanage-viraj <bhanagevirajvaibahv@gmail.com>
2025-03-17 16:31:46 +0530

    implentating AuthService in loginviewmodel

--- Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/ViewModels/LoginViewModel.swift
+++ Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/ViewModels/LoginViewModel.swift
@@ -1,11 +1,5 @@
-//
-//  LoginViewModel.swift
-//  Smart-Attendance-Tracker
-//
-//  Created by Viraj Bhanage on 17/03/25.
-//
-
 import SwiftUI
+
 class LoginViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
@@ -13,16 +7,27 @@ class LoginViewModel: ObservableObject {
     @Published var errorMessage: String?
     @Published var isLoggedIn: Bool = false
     
-    
-    
-    func login () {
+    func login() {
         guard !email.isEmpty, !password.isEmpty else {
             errorMessage = "Please enter email and password"
             return
         }
-        isLoading = true
-        // to call authservice
         
+        isLoading = true
+        
+      
+        AuthService.shared.login(email: email, password: password) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                
+                switch result {
+                case .success(let token):
+                    self?.isLoggedIn = true
+                    UserDefaults.standard.set(token, forKey: "jwt_token")
+                case .failure(let error):
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
     }
-    
 }
