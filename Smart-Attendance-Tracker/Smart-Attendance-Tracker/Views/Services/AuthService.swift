11f86fbb619ff6fc9a166892879afd57f1575ae7
bhanage-viraj <bhanagevirajvaibahv@gmail.com>
2025-03-20 23:30:35 +0530

    updated auth services  (Integrated Token Manager with auth services)

--- Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/Services/AuthService.swift
+++ Smart-Attendance-Tracker/Smart-Attendance-Tracker/Views/Services/AuthService.swift
@@ -5,58 +5,86 @@
 //  Created by Viraj Bhanage on 17/03/25.
 //
 
+
+
 import Foundation
 
 enum AuthError: Error {
-    case invaildCredentials
+    case invalidCredentials
     case invalidResponse
-    case invaildURL
-    
+    case invalidURL
+    case networkError(Error)
+    case noData
+    case decodingError
 }
 
 class AuthService {
     static let shared = AuthService()
-    private let baseURL = "http://3.95.186.173:8080/attendance/auth/login"
+    private let baseURL = "http://3.95.186.173:8080/attendance/auth"
+    
     private init() {}
-    func login(email: String , password: String, completion: @escaping (Result<String, Error>) -> Void) {
+    
+    // MARK: - Email & Password Authentication
+    func login(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
         guard let url = URL(string: "\(baseURL)/login") else {
-                   completion(.failure(NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-                   return
-               }
-        let body :[String : String] = ["email": email,"password": password]
+            completion(.failure(AuthError.invalidURL))
+            return
+        }
+        
+        let body: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
         let request = makeRequest(url: url, body: body)
-                
-        performRequest(request: request, completion: completion)
-        
-        
+        performRequest(request: request) { result in
+            switch result {
+            case .success(let token):
+                // Save token to TokenManager
+                TokenManager.saveToken(token)
+                completion(.success(token))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
     
+    // MARK: - Google Authentication
     func loginWithGoogle(token: String, completion: @escaping (Result<String, Error>) -> Void) {
-           guard let url = URL(string: "\(baseURL)/google") else {
-               completion(.failure(NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-               return
-           }
-           
-           let body: [String: String] = ["token": token]
-           let request = makeRequest(url: url, body: body)
-           
-           performRequest(request: request, completion: completion)
-       }
+        guard let url = URL(string: "\(baseURL)/google") else {
+            completion(.failure(AuthError.invalidURL))
+            return
+        }
+        
+        let body: [String: String] = ["token": token]
+        let request = makeRequest(url: url, body: body)
+        
+        performRequest(request: request) { result in
+            switch result {
+            case .success(let token):
+                // Save token to TokenManager
+                TokenManager.saveToken(token)
+                completion(.success(token))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
     
+    // MARK: - Logout
+    func logout() {
+        TokenManager.deleteToken()
+    }
     
-    
-    
-    private func makeRequest(url: URL, body: [String: String]) -> URLRequest {
-        var request = URLRequest(url:url)
-        request.httpMethod="POST"
+    // MARK: - Private Helper Methods
+    private func makeRequest(url: URL, body: [String: Any]) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
         return request
-
     }
-    
-    
-    
+    // update to handel https satus code
     private func performRequest(request: URLRequest, completion: @escaping (Result<String, Error>) -> Void) {
         // handel status code response
         URLSession.shared.dataTask(with: request) { data, response, error in
@@ -75,5 +103,8 @@ class AuthService {
         }.resume()
     }
     
-    
 }
+
+
+
+
